4f2ff336d36819545786f354e521aef3035d8ec8
Keshav Grover <keshavgrover84@yahoo.com>
2022-07-23 13:40:55 +0530

    Add First Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>
+                This is the title
+            </h1>
+            <p>
+                This is the Content.
+            </p>
+        </div>
+    );
+}
+
+export default Note;
